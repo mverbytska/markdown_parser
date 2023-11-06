b750aadc0ffa3414b8a74216060ee7920c4ac252
mverbytska <verbitskaya.mari@gmail.com>
2023-11-06 22:21:50 +0100

    Modify existing project files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 
 use pest_derive::Parser;
+use pest::Parser;
+use anyhow::anyhow;
 
 #[derive(Parser)]
 #[grammar = "./grammar.pest"]
 pub struct Grammar;
-  pub fn main() {
-     
+  pub fn main() -> anyhow::Result< () >{
+
+     Ok( () )
   }
+
+#[test]
+pub fn basic_test() -> anyhow::Result< () >{
+  let got = Grammar::parse(Rule::field, "121.87")?.next().ok_or_else(|| anyhow!("no pair"));
+  //assert_eq!();
+  dbg!(got);
+
+  Ok( () )
+}
