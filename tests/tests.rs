8423248104913e842e0ae11dfda5c7c51b1d718d
mverbytska <verbitskaya.mari@gmail.com>
2023-11-07 17:24:45 +0100

    Add simple tests for newly defined grammar

---
+++ tests/tests.rs
@@ -0,0 +1,29 @@
+//! Tests for markdown parser
+// TODO: implement more difficul ones
+
+#[cfg(test)]
+mod tests {
+    use pest_derive::Parser;
+    use pest::Parser;
+
+    #[derive(Parser)]
+    #[grammar = "../src/grammar.pest"] 
+    struct MarkdownParser;
+
+    #[test]
+    fn test_heading() {
+        assert!(MarkdownParser::parse(Rule::heading, "# This is a heading\n").is_ok());
+        assert!(MarkdownParser::parse(Rule::heading, "## This is another heading\n").is_ok());
+    }
+
+    #[test]
+    fn test_bold() {
+        assert!(MarkdownParser::parse(Rule::bold, "**This is bold text**").is_ok());
+    }
+
+    #[test]
+    fn test_italic() {
+        assert!(MarkdownParser::parse(Rule::italic, "*This is italic text*").is_ok());
+    }
+}
+
