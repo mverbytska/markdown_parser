dc5d214b91a477d5f6309cb8d0508ec4ed211872
mverbytska <verbitskaya.mari@gmail.com>
2023-11-16 08:36:06 +0100

    Add tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,23 +1,44 @@
 /// Tests for markdown parser
 
+#[path = "../src/lib.rs"]
+mod lib;
+use lib::to_html;
+
+
 #[cfg(test)]
-mod tests {
-    use pest_derive::Parser;
-    use pest::Parser;
 
-    #[derive(Parser)]
-    #[grammar = "../src/grammar.pest"] 
-    struct MarkdownParser;
 
+/// Heading test testing
     #[test]
-    fn test_heading() {
-        assert!(MarkdownParser::parse(Rule::heading, "# This is a heading\n").is_ok());
-        assert!(MarkdownParser::parse(Rule::heading, "## This is another heading\n").is_ok());
+    fn test_heading() -> anyhow::Result< () > {
+        assert_eq!(to_html("# This is a heading\n").unwrap(), "<h1>This is a heading</h1>");
+        assert_eq!(to_html("## This is another heading\n").unwrap(), "<h1>This is another heading</h1>");
+
+        Ok(())
     }
 
+/// Bold text testing
     #[test]
-    fn test_bold() {
-        assert!(MarkdownParser::parse(Rule::bold, "**This is bold text**").is_ok());
+    fn test_bold() -> anyhow::Result< () > {
+        assert_eq!(to_html("**This is bold text**").unwrap(), "<strong>This is bold text</strong>");
+
+        Ok(())
     }
-}
+
+/// Plain text testing
+    #[test]
+    fn test_text() -> anyhow::Result< () > {
+        assert_eq!(to_html("This is regular text").unwrap(), "<p>This is regular text</p>");
+
+        Ok(())
+    }
+
+
+/// Link text testing
+    #[test]
+    fn test_link() -> anyhow::Result< () >  {
+        assert_eq!(to_html("[Link](https://example.com)").unwrap(), "<a href=\"https://example.com\">Link</a>");
+
+        Ok(())
+    }
 
